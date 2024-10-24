ffa360bea4b57b24c7260ee724b8dc8d7ea26e88
perfectdude719 <mohamedhussein137841@gmail.com>
2024-10-25 00:41:34 +0300

    نمنت

---
+++ 8_puzzle.py
@@ -0,0 +1,5 @@
+class puzzle:
+    def __init__(self):
+        self.board=[[123],[456],[780]] # initialization for the board 
+    def user_input_states(self):
+        self.board=[input("please enter the initial state")]
